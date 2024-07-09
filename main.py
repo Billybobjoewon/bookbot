d9f2b4afee6f8a6dcb18354f55d838708069e740
Billybobjoewon <shanerich@comcast.net>
2024-07-09 19:40:34 -0400

    word count

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         print(file_contents)
+    words = file_contents.split()
+    count = 0
+    for word in words:
+        count += 1
+    print(count)
 
 main()
