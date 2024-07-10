44b8c4df26c3656d1ca8e3b6181859e743554e20
Billybobjoewon <shanerich@comcast.net>
2024-07-10 18:13:29 -0400

    proper output

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        #print(file_contents)
     words = file_contents.split()
     count = 0
     for word in words:
         count += 1
-    print(count)
+    # print(count)
     letters = {"a": 0, "b": 0, "c": 0, "d": 0,
      "e": 0, "f": 0, "g": 0, "h": 0, "i": 0,
       "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, 
@@ -17,5 +17,21 @@ def main():
     for char in lower_file:
         if char in letters:
             letters[char] += 1
-    print(letters)
+    #print(letters)
+    #directed to use function for sort key, functions have yet to be taught
+    def sort_on(dict):
+        return dict["num"]
+    sort_letters = []
+    for i in letters:
+        temp = {"let": i, "num": letters[i]}
+        sort_letters.append(temp)
+    sort_letters.sort(reverse=True, key=sort_on)
+    #print(sort_letters)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count} words found in the document\n")
+    for dict in sort_letters:
+        character = dict["let"]
+        number = dict["num"]
+        print(f"The {character} character was found {number} times")
+    print("--- End report ---")
 main()
